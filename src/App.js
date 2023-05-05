625714c3155251dd2a08008eaa997ae93a140ff9
minjiBaek <emilyback@naver.com>
2023-05-05 17:17:30 +0900

    평점 9 점 이상의 영화를 년도 순으로 정리하여 조회한 영화 소개페이지 -제목, 이미지, 설명, 성격(?) 표시 -타이틀 클릭시 영화 디테일 페이지로 이동하기 위해 react-router 설치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import './Movie';
+import Movie from './Movie';
 
 function App() {
+  const [loading, setloading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (
+      await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+      ).json();
+    setMovies(json.data.movies);
+    setloading(false);
+  }
+  useEffect( ()=> {
+    getMovies();
+  }, []);
   return (
-    <div className="App">
-     <h1>Welcome</h1>
+    <div>
+      {loading ? ( <h1>loading...</h1> ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie 
+              key={movie.id}
+              coverImg={movie.medium_cover_image} 
+              genres={movie.genres} 
+              summary={movie.summary} 
+              title={movie.title}   
+              />
+          )
+
+          )}
+          </div>
+      )}
     </div>
   );
 }
